53be15842723e956c4d87a462d788b97df1af5b3
virginiegt085-lab <virginie.gt085@gmail.com>
2025-09-04 16:48:56 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const Stripe = require("stripe");
+const app = express();
+
+const stripe = Stripe(process.env.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); // clé secrète via variable d'env
+
+app.use(express.json());
+
+app.post("/create-intent", async (req, res) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 6000, // en centimes => 100,00 €
+      currency: "eur",
+      capture_method: "manual", // 👉 empreinte CB
+      payment_method_types: ["card"],
+    });
+    res.json({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.get("/", (req, res) => res.send("API Stripe OK"));
+
+app.listen(6000, () => console.log("Server running on port 10000"));
